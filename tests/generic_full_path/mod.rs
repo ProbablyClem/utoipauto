a755bf979a57b0730b67db26448de76eb9b71d9a
Denux <timon.klinkert@denux.dev>
2024-03-06 19:11:19 +0100

    cleaned up `mod.rs` files

--- tests/generic_full_path/mod.rs
+++ tests/generic_full_path/mod.rs
@@ -1,10 +1,3 @@
-use utoipa::OpenApi;
-use utoipauto_macro::utoipauto;
-
 mod more_schemas;
 mod schemas;
-
-#[utoipauto(paths = "./tests/generic_full_path")]
-#[derive(OpenApi)]
-#[openapi(info(title = "Percentage API", version = "1.0.0"))]
-pub struct CrateApiDocs;
+mod test;
