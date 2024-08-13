cba4458b75d14ec1816d6af347699c8cb2824b0d
Denux <timon.klinkert@denux.dev>
2024-08-13 07:22:16 +0200

    added separate workspace for acceptence tests

---
+++ crate_tests/README.md
@@ -0,0 +1,4 @@
+# Crate tests for utoipauto
+
+This workspace contains multiple crates that tests different aspects about the utoipauto macro.  
+These tests should represent how the user is expected to use the library.
--- crate_tests/crate_segment_path/crate_folder/sub_folder/test.rs
+++ crate_tests/crate_segment_path/crate_folder/sub_folder/test.rs
@@ -1,9 +1,10 @@
 use utoipa::OpenApi;
-use utoipauto_macro::utoipauto;
+use utoipauto::utoipauto;
 
-#[utoipauto(paths = "( ./crate_folder/sub_folder/paths.rs from crate::sub_folder )")]
+#[utoipauto(paths = "( ./crate_segment_path/crate_folder/sub_folder/paths.rs from crate::sub_folder )")]
 #[derive(OpenApi)]
 #[openapi(info(title = "Percentage API", version = "1.0.0"))]
+#[allow(dead_code)]
 pub struct CrateInAnotherPath {}
 
 #[test]
